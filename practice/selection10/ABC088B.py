f0a83710df4aa0403fe170aad1d1974a786236a1
zawa-kun <shult.0219@gmail.com>
2025-02-13 13:58:43 +0900

    practice: ABC088B

--- practice/selection10/ABC088B.py
+++ practice/selection10/ABC088B.py
@@ -1,11 +1,39 @@
-from typing import List
-def partition(numbers: List[int], high: int, low: int):
-def quick_sort(numbers: List[int]) -> List[int]:
+N = int(input())
+a = list(map(int, input().split()))
+a.sort(reverse=True) #降順にソート.
+
+Alice = 0 #アリスの点数.
+Bob = 0 #ボブの点数.
+
+flag = True #カードを取る人をフラグで管理.
+
+#カードをN回ドロー.
+for _ in range(N):
+    if flag:
+        Alice += a[0]
+        a.pop(0) #加算したものから順番に取り除いていく.
+        flag = False
+    else:
+        Bob += a[0]
+        a.pop(0)
+        flag = True
+    
+print(Alice - Bob) #AliceとBobの差.
 
 
-if __name__ == '__main__':
-    #合計len(N)回カードを引く.
-    #それぞれの最大点を比べる.
-    # N = int(input())
-    a = [map(int, input().split())]
-    quick_sort(a)
+
+"""
+コードレビュー
+1 pop(0)は非効率
+・pop(0)を使うと非効率.リストの先頭要素を削除する為、その度二シフトをする必要があり、O(N)の計算量がかかる.
+
+2 フラグを使わず、enumrateを使うト簡潔になる.
+ループの偶奇でAlice/Bobを判断した方がシンプルになる.
+例⇓⇓⇓
+# 偶数番目 (Alice) / 奇数番目 (Bob) で分ける
+for i, card in enumerate(a):
+    if i % 2 == 0:
+        Alice += card
+    else:
+        Bob += card
+"""
